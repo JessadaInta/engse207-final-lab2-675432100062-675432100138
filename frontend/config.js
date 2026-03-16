7f3182a8d4068e3b1bee379ccce9f12edf5e5d9e
JessadaInta <nnicenicekung@gmail.com>
2026-03-16 21:56:56 +0700

    add frontend config and deploy screnshot

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  AUTH_URL: 'https://auth-service-production-f754.up.railway.app',
+  TASK_URL: 'https://task-service-production-5d53.up.railway.app',
+  USER_URL: 'https://user-service-production-5a00.up.railway.app'
+};
