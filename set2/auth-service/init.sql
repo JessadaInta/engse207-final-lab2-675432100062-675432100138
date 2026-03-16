ec86d74a2530186dd96d1ee544ee8ae8577cd8c8
JessadaInta <nnicenicekung@gmail.com>
2026-03-16 16:20:20 +0700

    add seed data

--- set2/auth-service/init.sql
+++ set2/auth-service/init.sql
@@ -8,14 +8,11 @@ CREATE TABLE IF NOT EXISTS users (
   last_login    TIMESTAMP
 );
 
-INSERT INTO users (username,email,password_hash,role)
-VALUES
-(
-'admin',
-'admin@lab.local',
-'$2b$10$f3Aha3p9oVMqXG4Q6DnE7uiI1uxpOopewnVHPjJ.xg9leMm8h8c2S',
-'admin'
-);
+INSERT INTO users (username,email,password_hash,role) VALUES
+('admin','admin@lab.local','$2b$10$f3Aha3p9oVMqXG4Q6DnE7uiI1uxpOopewnVHPjJ.xg9leMm8h8c2S','admin'),
+('user-001', 'alice@example.com','$2a$10$aOp3jwB6LvbUSbkz1wJEgu1dmjxoocG8/JmQlc/Nev20.SWH0sQ9K', 'member'),
+('user-002', 'bob@example.com','$2a$10$aOp3jwB6LvbUSbkz1wJEgu1dmjxoocG8/JmQlc/Nev20.SWH0sQ9K', 'member'),
+('admin2', 'admin2@example.com','$2a$10$aOp3jwB6LvbUSbkz1wJEgu1dmjxoocG8/JmQlc/Nev20.SWH0sQ9K', 'admin');
 
 CREATE TABLE IF NOT EXISTS logs (
   id         SERIAL PRIMARY KEY,
--- set2/user-service/init.sql
+++ set2/user-service/init.sql
@@ -10,8 +10,11 @@ CREATE TABLE IF NOT EXISTS user_profiles (
   updated_at   TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO user_profiles (user_id,username,email,role)
-VALUES (1,'admin','admin@lab.local','admin')
+INSERT INTO user_profiles (user_id,username,email,role) VALUES 
+(1,'admin','admin@lab.local','admin'),
+(2, 'Alice Smith', 'alice@example.com', 'member'),
+(3, 'Bob Jones',   'bob@example.com',   'member'),
+(4, 'admin2', 'admin2@example.com', 'admin')
 ON CONFLICT (user_id) DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS logs (
